c4ec3ec541741e0023d672359f213761764152cd
saburo.kirameki <ec2-user@ip-172-31-5-128.us-east-2.compute.internal>
2018-06-19 03:25:04 +0000

    first commit

---
+++ app/Micropost.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function favorito()
+    {
+        return $this->belongsToMany(User::class, 'user_favorite', 'favorite_id', 'user_id');
+    }
+}
